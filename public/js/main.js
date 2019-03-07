a56973e6bec2923e4047d906dfd5696b25e96daf
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-07 13:37:11 -0500

    testable solution

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,12 @@
 // initialize variables
-var httprequest;
+var httpRequest = null;
 var monday = document.getElementById("monday");
+var content;
 
 monday.addEventListener("click", showRoutines);
 
 function showRoutines(){
-    httprequest = new XMLHttpRequest();
+    httpRequest = new XMLHttpRequest();
 
     // gaurd.
     if(!httpRequest){
@@ -14,18 +15,37 @@ function showRoutines(){
     }
 
     // set callback handler.
-    httpRequest.onreadystatechange = alertContents;
+    httpRequest.onreadystatechange = handleRequest;
     httpRequest.open('GET', 'http://localhost/my_fitness/monday/read.php');
     httpRequest.setRequestHeader('Content-type', 'application/json');
-    httpRequest.send(content);
+    httpRequest.send();
 }
 
 function handleRequest(){
     if(httpRequest.readyState === XMLHttpRequest.DONE){
         if(httpRequest.status === 200){
-            alert(httpRequest.responseText);
+            content = JSON.parse(httpRequest.response);
+            render(content);
         } else {
             alert("Error in the request");
         }
     }
+}
+
+function render(content){
+    content.records.forEach(element => {
+        var id = element.id;
+        var reps = element.reps;
+        var exercise = element.exercise;
+        appendRoutine(id, reps, exercise);
+    });
+}
+
+function appendRoutine(id, reps, exercise){
+    var container = document.createElement('div');
+    container.setAttribute('id', id);
+    var row = document.createElement('p');
+    row.innerHTML = reps + " : " + exercise;
+    container.appendChild(row);
+    monday.parentNode.appendChild(container);
 }
--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,4 +1,5 @@
 </body>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="./../public/js/main.js"></script>
 </html>
